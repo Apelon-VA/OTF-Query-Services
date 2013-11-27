795dc48c1cc5d21e0a3ba9f66e2ce4271bdde430
Aimee Furber <furber@informatics.com>
2013-11-26 17:30:45 -0800

    and some more debugging

--- query-service/src/main/java/org/ihtsdo/otf/query/rest/server/SwaggerServlet.java
+++ query-service/src/main/java/org/ihtsdo/otf/query/rest/server/SwaggerServlet.java
@@ -32,6 +32,7 @@ public class SwaggerServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config){
         String url = System.getProperty("host.url");
+        url = "http://this-should-never-be-used";
         if(url.contains("/otf")){
             url = url.substring(0, url.indexOf("/otf") + 4) + "/query-service";
         }
